ed696f6a8436bd91a1c9546a5754ef53557e591e
swapv4659 <59692573+swapv4659@users.noreply.github.com>
2020-02-26 08:14:06 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,163 +1,111 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<style type="text/css">
-		.container{
-			padding: 20px;
-			width: 75%;
-			margin: 15px;
-		}
-		.row:after {
- 			 content: "";
-  			display: table;
-  			clear: both;
-		}
-		.col-25 {
-  			float: left;
- 			width: 15%;
-  			margin-top: 9px;
-		}
-		.col-75 {
-  			float: left;
-  			width: 85%;
-  			margin-top: 9px;
-		}
-		input[type="text"]{
-			width:35%;
-		}
-		input[type="submit"]{
-			margin-top: 25px;
-			height:5%;
-			width: 10%;
-			float: left;
-		}
-	</style>
-	<script type="text/javascript">
-		function validation() {
-			// body...
-			var phone=document.myForm.pnumber.value;
-			var first=document.myForm.fname.value;
-			var last=document.myForm.lname.value;
-			var email=document.myForm.email.value;
-	
-			if(first=="")
-			{
-				alert("Enter FIRST NAME")
-				return false;
-			}
-			if(last=="")
-			{
-				alert("Enter LAST NAME")
-				return false;
-			}
-			if(phone=="")
-			{
-				alert("Enter Phone Number")
-				return false;
-			}
-			if(isNaN(phone) || phone.indexOf(" ")!=-1)
-			{
-				alert("Enter Numeric Value")
-				return false;
-			}
-			if (phone.length>10 || phone.length<10)
-           {
-                alert("Enter 10 characters");
-                return false;
-           }
-			if(email=="")
-			{
-				alert("Enter EMAIL")
-				return false;
-			}
-			var atposition=email.indexOf("@");  
-			var dotposition=email.lastIndexOf(".");  
-			if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length){  
-  			alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
-  			return false;  
-  			}  
-		}
-	</script>
+  <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  .container-fluid{
+    height: 100%;
+    margin-top: 5%;
+    margin-bottom: 5%;
+  }
+  .block1 {
+    max-height: 100%;
+    background-color: #b3b3b3;
+    border-style: outset;
+  }
+  .block{
+    height: auto;
+    background: #ffffff;
+  }
+  img{
+    width: 100%;
+  }
+  .fontstyle{
+    font-size: 2vw;
+    margin-bottom: -10%;
+    margin-top: 10%;
+    color: #ffffff;
+    text-decoration: none;
+  }
+  .head{
+    font-size: 6vw;
+    margin-top: 3%;
+    margin-bottom: 3%;
+  }
+  .image1{
+    position: relative;
+  }
+  .secondtext{
+    font-size: 3.5vw;
+  }
+  .one{
+    width: 40%;
+    margin-top: 17%;
+  }
+  .two{
+    background: #000000;
+  }
+  .image2{
+    border-right:1px solid #ffffff;
+    margin-top: 2%;
+  }
+  .img{
+    position: absolute;
+    top: 20%;
+    left: 20%;
+    width: 40%;
+  }
+  </style>
 </head>
+
 <body>
-	<form name="myForm" method="post" onsubmit="return validation()">
-	<div class="container">
-		<div class="row">
-			<div class="col-25">
-			FIRST NAME :
-			</div>
-			<div class="col-75">
-				<input type="text" name="fname" id="fname">
-			</div>
-				<div class="col-25">
-			LAST NAME :
-			</div>
-			<div class="col-75">
-				<input type="text" name="lname" id="lname">
-			</div>
-			<div class="col-25">
-			PHONE NUMBER :
-			</div>
-			<div class="col-75">
-				<input type="text" name="pnumber" id="pnumber">
-			</div>
-			<div class="col-25">
-			EMAIL :
-			</div>
-			<div class="col-75">
-				<input type="text" name="email" id="email">
-			</div>
-		</div>
-		<input type="Submit" name="submit">
-	</div>
-</form>
+  <div class="container-fluid block">
+    <div class="row no-gutters">
+
+      <?php 
+
+      $conn=mysqli_connect('localhost','root','','emp');
+      $result=mysqli_query($conn,"SELECT * FROM emp2");
+      while(mysqli_fetch_array($result))
+      {
+        ?>
+      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 block1">
+        <div class="text-center head">HOTEL KRISHNA</div>
+         <div class="text-center image1"><img src="1.jpg"><img src="6.png" class="img"></div>
+          <div class="text-center secondtext">Table No: <?php echo $row['empid']; ?></div>
+          <div class="text-center secondtext">Scan QR Code To Order FOOD</div>
+          <div class="row no-gutters two">
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-6 text-center">
+                  <img src="4.png" class="image2">
+                </div>
+              <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-6">
+                <div class="text-center"><a href="https://play.google.com/store" class="fontstyle">Available ON!!</a></div>
+                  <div class="row no-gutters">
+                  <div class="col-xs-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-6 text-center"><img src="4.png"></div>
+                  <div class="col-xs-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-6 text-center"><img src="apple.png" class="one"></div>
+                </div>
+              </div>
+            </div>
+        </div>
+    <?php  } ?>
+    </div>
+  </div>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>    
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-<?php
-$conn=new mysqli("localhost","root","","form");
-if($conn->connect_error)
-{
-	echo "Failed To Connect".$mysqli->connect_error;
-	exit();
-}
-if(isset($_POST['submit']))
-{
-
-$fname=$_POST['fname'];
-$lname=$_POST['lname'];
-$phone=$_POST["pnumber"];
-$email=$_POST["email"];
-
-$sql="INSERT INTO info(FName,Lname,PNumber,Email)VALUES('$fname','$lname','$phone','$email')";
-
-if(mysqli_query($conn,$sql))
-{
-	echo "Data Added";
-}
-}
-
-if(isset($_POST['submit']))
-{
-	$result=mysqli_query($conn,"SELECT * from info");
-
-	echo "<table border='1'>
-			<tr>
-			<th>Sr.Number</th>
-			<th>First Name</th>
-			<th>Last Name</th>
-			<th>Phone Number</th>
-			<th>Email</th>
-			</tr>";
-		$count=0;
-	while($row=mysqli_fetch_array($result))
-	{
-		echo "<tr>";
-		echo "<td>".++$count."</td>";
-		echo "<td>".$row['FName']."</td>";
-		echo "<td>".$row['Lname']."</td>";
-		echo "<td>".$row['PNumber']."</td>";
-		echo "<td>".$row['Email']."</td>";
-	}
-}
-?>
-	
